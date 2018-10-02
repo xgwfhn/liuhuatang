0306a2ba74cbfd91ec14db058c0bdcfafd75e57e
xgwfhn <xgwfhn@126.com>
2018-10-02 20:37:16 +0800

    修改分类右侧显示样式

--- src/main/java/com/lht/common/controller/CategoryController.java
+++ src/main/java/com/lht/common/controller/CategoryController.java
@@ -6,10 +6,14 @@ public class CategoryController extends Controller {
 
 	public void index() {
 		renderText("此方法是一个action");
-		render("category.html");
+		render("category1.html");
 	}
 
 	public void search() {
 		render ("search.html"); 
+	}
+	
+	public void searchResult() {
+		render ("searchResult.html"); 
 	}
 }
